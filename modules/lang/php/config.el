0e161454eb275e5233452e8e755966d950763967
Ellis Kenyo <ellis@coreblue.co.uk>
2021-07-25 11:04:14 +0100

    feature: Improve PHP module
    * Add more detailed README
    * Add `composer.el`
    * Create a minor mode for phpunit/docker tests

--- modules/lang/php/config.el
+++ modules/lang/php/config.el
@@ -3,6 +3,9 @@
 (defvar +php--company-backends nil
   "List of company backends to use in `php-mode'.")
 
+(defvar +php/default-docker-container "php-fpm"
+  "The default docker container to run commands in")
+
 (after! projectile
   (add-to-list 'projectile-project-root-files "composer.json"))
 
@@ -15,7 +18,7 @@
   :config
   ;; Disable HTML compatibility in php-mode. `web-mode' has superior support for
   ;; php+html. Use the .phtml extension instead.
-  (setq php-template-compatibility nil)
+  (setq php-mode-template-compatibility nil)
 
   (set-docsets! 'php-mode "PHP" "PHPUnit" "Laravel" "CakePHP" "CodeIgniter" "Doctrine_ORM")
   (set-repl-handler! 'php-mode #'php-boris)
@@ -23,7 +26,7 @@
   (set-formatter! 'php-mode #'php-cs-fixer-fix)
   (set-ligatures! 'php-mode
     ;; Functional
-    :lambda "function()"
+    :lambda "function()" :lambda "fn"
     :def "function"
     ;; Types
     :null "null"
@@ -126,6 +129,21 @@
   :when (featurep! +hack)
   :mode "\\.hh$")
 
+(use-package! composer
+  :after php-mode
+  :config
+  (map! :localleader
+        :map php-mode-map
+        :prefix ("c" . "composer")
+        "c" #'composer
+        "i" #'composer-install
+        "r" #'composer-require
+        "u" #'composer-update
+        "d" #'composer-dump-autoload
+        "s" #'composer-run-script
+        "v" #'composer-run-vendor-bin-command
+        "o" #'composer-find-json-file
+        "l" #'composer-view-lock-file))
 
 ;;
 ;; Projects
@@ -137,3 +155,10 @@
 (def-project-mode! +php-composer-mode
   :modes '(web-mode php-mode)
   :files ("composer.json"))
+
+(def-project-mode! +phpunit-docker-compose-mode
+  :modes '(php-mode docker-compose-mode)
+  :files (and "phpunit.xml" "docker-compose.yml")
+  :on-enter
+  (setq phpunit-args `("exec" ,+php/default-docker-container "php" "vendor/bin/phpunit")
+        phpunit-executable (executable-find "docker-compose")))
